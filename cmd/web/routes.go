f8a266d20baa11f06c0b72ef9b598d81ce2f7970
mohamad mr <mohamadmis92@gmail.com>
2023-03-15 14:57:45 +0330

    middlewares added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,14 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-// The routes() method returns a servemux containing our application routes.
-func (app *application) routes() *http.ServeMux {
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return mux
+
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	
+	return standard.Then(mux)
 }
+
+// You don’t need to use justinas/alice package, but the reason I recommend it is because it makes it easy
+// to create composable, reusable, middleware chains — and that can be a real help as your
+// application grows and your routes become more complex. The package itself is also small and
+// lightweight, and the code is clear and well written.

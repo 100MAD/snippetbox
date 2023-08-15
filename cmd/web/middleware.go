61717c7bb7a9154d60560cc0c329c9886eafcd39
mohamad mr <mohamadmis92@gmail.com>
2023-08-15 11:46:01 +0430

    unit tests added

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -28,13 +29,11 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Create a deferred function (which will always be run in the event
-		// of a panic as Go unwinds the stack).
+
 		defer func() {
-			// Use the builtin recover function to check if there has been a
-			// panic or not.
+
 			if err := recover(); err != nil {
-				// Set a "Connection: close" header on the response.
+
 				w.Header().Set("Connection", "close")
 				app.serverError(w, fmt.Errorf("%s", err))
 			}
@@ -64,4 +63,27 @@ func noSurf(next http.Handler) http.Handler {
 		Secure:   true,
 	})
 	return csrfHandler
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := app.sessionManager.GetInt(r.Context(), "authenticatedUserID")
+		if id == 0 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		exists, err := app.users.Exists(id)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		if exists {
+			ctx := context.WithValue(r.Context(), isAuthenticatedContextKey, true)
+			r = r.WithContext(ctx)
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module snippetbox.mmdmr.net
 go 1.20
 
 require (
-	github.com/alexedwards/scs/mysqlstore v0.0.0-20230305153148-62e546ce9d2d // indirect
-	github.com/alexedwards/scs/v2 v2.5.1 // indirect
-	github.com/go-playground/form/v4 v4.2.0 // indirect
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/justinas/alice v1.2.0 // indirect
-	github.com/justinas/nosurf v1.1.1 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
+	github.com/alexedwards/scs/mysqlstore v0.0.0-20230305153148-62e546ce9d2d
+	github.com/alexedwards/scs/v2 v2.5.1
+	github.com/go-playground/form/v4 v4.2.0
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/justinas/alice v1.2.0
+	github.com/justinas/nosurf v1.1.1
+	golang.org/x/crypto v0.7.0
 )
